3ecd89820595833b71164a95140e7a15e119f4d7
Katarzyna Balicka <kbbali@gmail.com>
2021-04-21 22:59:42 +0000

    preparing js file

--- simple_editor.js
+++ simple_editor.js
@@ -3,3 +3,8 @@ const buttonSave = document.querySelector(".save")
 
 console.log(buttonLoad)
 console.log(buttonSave)
+
+
+function showDetails(animal) {
+    var animalType = animal.getAttribute("data-animal-type");
+    alert("The " + animal.innerHTML + " is a " + animalType + ".");}
